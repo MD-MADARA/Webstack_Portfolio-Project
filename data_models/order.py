ac51b2718be26d777b84b6527e57be3276c3fea3
MD-MADARA <mouadotaku9421@gmail.com>
2024-07-07 17:13:19 +0100

    define a relationship between order table and order_items table

--- data_models/order.py
+++ data_models/order.py
@@ -2,7 +2,7 @@
 """ holds class Category"""
 from data_models.shared import SharedBase, Base
 from sqlalchemy import Column, String, Float, Integer, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 
 class Order(SharedBase, Base):
@@ -11,3 +11,8 @@ class Order(SharedBase, Base):
     total_price = Column(Float, nullable=False, default=0)
     status = Column(String(50), nullable=False, default="pending")
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    order_items = relationship(
+        "OrderItem",
+        backref="order",
+        cascade="all, delete, delete-orphan"
+    )
