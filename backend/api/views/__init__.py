e50ccfb3208a8c4bde41790f70878d884955dc91
MD-MADARA <mouadotaku9421@gmail.com>
2024-07-07 21:37:51 +0100

    register blue print and build api status view

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -4,3 +4,10 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api')
 
+from backend.api.views.api_status_view import *
+# from backend.api.views.user_view import *
+# from backend.api.views.product_view import *
+# from backend.api.views.order_view import *
+# from backend.api.views.order_items_view import *
+# from backend.api.views.login_view import *
+# from backend.api.views.cart_view import *
--- backend/api/views/api_status.py
+++ backend/api/views/api_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ User view """
-from api.views.__init__ import app_views
+from backend.api.views.__init__ import app_views
 from flask import jsonify, make_response
-from models import storage
-from models.product import Product
-from models.user import User
+from backend import storage
+from backend.data_models.product import Product
+from backend.data_models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
