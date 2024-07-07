564af1fa6a2ec0e0587200a4324048eca1c34636
MD-MADARA <mouadotaku9421@gmail.com>
2024-07-07 17:03:59 +0100

    add product model

---
+++ data_models/product.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" holds class User"""
+from data_models.shared import SharedBase, Base
+from sqlalchemy import Column, String, Integer, CheckConstraint, Float
+
+
+class Product(SharedBase, Base):
+    """Product Table"""
+    __tablename__ = 'products'
+    title = Column(String(256), nullable=False)
+    price = Column(Float, nullable=False)
+    description = Column(String(1024), nullable=False)
+    discount = Column(Integer, CheckConstraint('discount <= 100'), default=0)
+    category_name = Column(String(127), nullable=False)
+    category_type = Column(String(127), nullable=False)
+    img_url = Column(String(256))
