f8c30a0348e698bde278bdd99101a3b53df6907f
MD-MADARA <mouadotaku9421@gmail.com>
2024-07-07 22:40:19 +0100

    get user by email method

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -5,7 +5,7 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api')
 
 from backend.api.views.api_status import *
-# from backend.api.views.user_view import *
+from backend.api.views.user_view import *
 # from backend.api.views.product_view import *
 # from backend.api.views.order_view import *
 # from backend.api.views.order_items_view import *
