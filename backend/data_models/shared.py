458cc2cbb434b78fea85c671656dacc445f4c8ed
MD-MADARA <mouadotaku9421@gmail.com>
2024-07-08 07:09:46 +0100

    fix shared model

--- backend/data_models/shared.py
+++ backend/data_models/shared.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
 """This module defines a shared class for all models"""
 from datetime import datetime
-from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy import Column, DateTime, String
 from sqlalchemy.orm import declarative_base
-
+from uuid import uuid4
+from pprint import pprint
 Base = declarative_base()
 
 
 class SharedBase:
     """A base class for all models"""
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-
-    created_date = Column(DateTime, default=datetime.now(), nullable=False)
-
-    updated_date = Column(DateTime, default=datetime.now(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Initialization of the Shared class model"""
-        if kwargs:
-            time = '%Y-%m-%dT%H:%M:%S.%f'
-            if kwargs.get("created_date", None) and type(self.created_date) is str:
-                self.created_date = datetime.strptime(kwargs["created_date"], time)
-            else:
-                self.created_date = datetime.now()
-            if kwargs.get("updated_date", None) and type(self.updated_date) is str:
-                self.updated_date = datetime.strptime(kwargs["updated_date"], time)
-            else:
-                self.updated_date = datetime.now()
-            for key, value in kwargs.items():
-                if key not in ["__class__", "created_date", "updated_date"]:
-                    if hasattr(self, key):
-                        setattr(self, key, value)
+        self.id = Column(String(128), primary_key=True, default=str(uuid4()))
+
+        self.created_date = Column(DateTime, default=datetime.now(), nullable=False)
+
+        self.updated_date = Column(DateTime, default=datetime.now(), nullable=False)
+
+        for key, value in kwargs.items():
+            if key not in ["__class__", "created_date", "updated_date"]:
+                if hasattr(self, key):
+                    setattr(self, key, value)
 
     def save(self):
         """Updates updated_date with current time when instance is changed"""
@@ -58,3 +50,4 @@ class SharedBase:
             if isinstance(value, datetime):
                 dictionary[key] = datetime.isoformat(value)
         return dictionary
+
